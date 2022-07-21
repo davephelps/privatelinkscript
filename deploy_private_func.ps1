486ed93883221647653d922947668737c8424a23
davephelps <31920274+davephelps@users.noreply.github.com>
2022-07-21 01:12:45 +0100

    Add files via upload

---
+++ deploy_private_func.ps1
@@ -0,0 +1,6 @@
+﻿$ResourceGroupName="privatefunc5"
+$location="northeurope"
+$appName='PrivateFunctionApp2'
+az group create --name $ResourceGroupName --location $location
+az deployment group create --resource-group $ResourceGroupName --template-file private_func.bicep --parameters functionAppName=$appName location=$location planResourceGroup=privatefunc functionAppPlanName=PrivateFunctionAppPlan5
+
